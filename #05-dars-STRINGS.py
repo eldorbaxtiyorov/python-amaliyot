bdb26e7af43403de0630e2f676febaef0b922da7
Eldor Baxtiyorov <91687326+eldorbaxtiyorov@users.noreply.github.com>
2021-09-30 22:06:46 +0500

    Add files via upload

---
+++ #05-dars-STRINGS.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 29 22:21:43 2021
+
+@author: User
+"""
+#1-mashq
+"""
+kocha="Bog'bon"
+mahalla="Sag'bon"
+tuman='Bodomzor'
+viloyat='Samarqand'
+
+print(kocha+' '+"ko'chasi,",mahalla+' '+"mahallasi,",tuman+' '+"tumani,",viloyat+' '+"viloyati")
+
+#2-mashq
+kocha=input("Ko'cha nomini kiriting:")
+mahalla=input("Mahalla nomini kiriting:")
+tuman=input("Tuman nomini kiriting:")
+viloyat=input("Viloyat nomini kiriting:")
+
+print(kocha+' '+"ko'chasi,",mahalla+' '+"mahallasi,",tuman+' '+"tumani,",viloyat+' '+"viloyati")
+
+
+#3-mashq
+kocha=input("Ko'cha nomini kiriting:")
+mahalla=input("Mahalla nomini kiriting:")
+tuman=input("Tuman nomini kiriting:")
+viloyat=input("Viloyat nomini kiriting:")
+
+print(kocha+' '+"ko'chasi,\n",mahalla+' '+"mahallasi,\n",tuman+' '+"tumani,\n",viloyat+' '+"viloyati")
+
+#4-mashq
+kocha=input("Ko'cha nomini kiriting:")
+mahalla=input("Mahalla nomini kiriting:")
+tuman=input("Tuman nomini kiriting:")
+viloyat=input("Viloyat nomini kiriting:")
+
+manzil=f"{kocha} ko'chasi,{mahalla} mahallasi,{tuman} tumani,{viloyat} viloyati"
+print(manzil.upper())
+print(manzil.lower())
+print(manzil.capitalize())
+print(manzil.title())
+"""
